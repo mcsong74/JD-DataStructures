16c800768ed7d54c0299afea31547af004cc0a92
ssonata74 <ssonata@gmail.com>
2020-12-09 20:14:39 -0500

    Datastructure - heap

---
+++ DataStructures-07-Heap/src/HeapMainApp.java
@@ -0,0 +1,14 @@
+public class HeapMainApp
+{
+    public static void main(String[] args)  {
+        MyHeap myHeap=new MyHeap();
+        myHeap.insert(100);
+        myHeap.insert(91);
+        myHeap.insert(101);
+        myHeap.insert(23);
+        myHeap.insert(2);
+        myHeap.insert(32);
+
+        myHeap.print();
+    }
+}
