4c0fa2ed7af7d719f6331ec9c98a3cb2cd024726
ssonata74 <ssonata@gmail.com>
2020-12-29 20:08:15 -0500

    Datastructure - Sorting, Insertion Sort

--- DataStructures-09-Sorting/src/MyInsertionSort.java
+++ DataStructures-09-Sorting/src/MyInsertionSort.java
@@ -2,8 +2,18 @@ public class MyInsertionSort {
 
     public static int[] insertionSort(int[] array){
 
+        for(int i=1; i<array.length; i++){
+            int current=array[i];
+            int j=i-1;
 
+            while(j>=0 && array[i]>current){
+                //shifting operation here
+                array[j+1]=array[j];
+                j--;
+            }
+            array[j+1]=current;
 
+        }
 
         return array;
     }
