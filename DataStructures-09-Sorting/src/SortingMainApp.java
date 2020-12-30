fa988a117154d34790d217ea0d166373be352821
ssonata74 <ssonata@gmail.com>
2020-12-29 19:28:58 -0500

    Datastructure - sorting, Bubble, selection

---
+++ DataStructures-09-Sorting/src/SortingMainApp.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class SortingMainApp {
+    public static void main(String[] args) {
+        var myArr=new int[]{8,2,4,1,3};
+
+        System.out.println("Bubble sorted= "+Arrays.toString(MyBubbleSort.bubbleSort(myArr)));
+
+        System.out.println("Selection sorted = "+Arrays.toString(MySelectionSort.selectionSort(myArr)));
+    }
+}
